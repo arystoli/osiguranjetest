268aa19c38613ddfc6d1874f78865cc7df7fadbe
gFleka <nkrizanec@riteh.hr>
2017-03-07 03:44:07 +0100

    Cijena Premija changed to display

--- resources/views/layouts/cijene.blade.php
+++ resources/views/layouts/cijene.blade.php
@@ -5,7 +5,7 @@
 			<div class="panel panel-danger">
 				<div class="panel-heading"><h4>Euroherc</h4></div>
 				<div class="panel-body">
-					<p>Temeljna premija: {{ $polica->Data->TemeljnaPremija }}</p>
+					<p>Temeljna premija: {{ $polica->Data->Premija }}</p>
 					<p><a href="{{ url('/getPolicaOnePage') }}">Izrada</a></p>
 				</div>
 			</div>
